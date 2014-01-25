169cbfbe9748d8a8d43690e7dcb2539355c44cd5
Lescieux Matthieu <MatthieuLescieux@hotmail.fr>
2014-01-25 23:58:26 +0100

    Connexion TCP côté serveur
    Gestion des clients, réception des paquets et début du traitement

---
+++ unpointzerosharp/Answers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace unpointzerosharp
+{
+    public static class Answers
+    {
+        public enum AnswerMethods {
+            Login,
+            Register,
+            NewCharacter,
+        };
+
+        public static void Login(byte index, byte[] data)
+        {
+            //A faire
+        }
+
+        public static void Register(byte index, byte[] data)
+        {
+            //A faire
+        }
+
+        public static void NewCharacter(byte index, byte[] data)
+        {
+            //A faire
+        }
+    }
+}
---
+++ unpointzerosharp/Network.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace unpointzerosharp
+{
+    public class Network
+    {
+        public const int PORT = 4000;
+
+        private TcpListener tcpListener;
+        private Thread listenThread;
+        private Dictionary<byte, Action<byte, byte[]>> answerHandler;
+
+        public Network()
+        {
+            InitializeAnswers();
+            tcpListener = new TcpListener(IPAddress.Any, PORT);
+            listenThread = new Thread(new ThreadStart(ListenClients));
+            listenThread.Start();
+        }
+
+        private void ListenClients()
+        {
+            tcpListener.Start();
+
+            while (true)
+            {
+                Console.WriteLine("En attente de connexion ...");
+                TcpClient tcpClient = tcpListener.AcceptTcpClient();
+
+                Thread clientThread = new Thread(new ParameterizedThreadStart(HandleClient));
+                clientThread.Start(tcpClient);
+            }
+        }
+
+        private void HandleClient(object client)
+        {
+            TcpClient tcpClient = (TcpClient)client;
+            Console.WriteLine("Connexion acceptée ...");
+
+            NetworkStream networkStream = tcpClient.GetStream();
+
+            byte[] message = new byte[4096];
+
+            while (true)
+            {
+                try
+                {
+                    ReadStream(networkStream, message);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Erreur lors de la réception d'un paquet ... Erreur : " + e.Message);
+                }
+            }
+        }
+
+        private void ReadStream(NetworkStream networkStream, byte[] message)
+        {
+            int index = 0;
+            index = networkStream.Read(message, index, 1);
+
+            if (index == 0)
+            {
+                Console.WriteLine("Déconnexion d'un client ...");
+            }
+            else
+            {
+
+            }
+        }
+
+        private void InitializeAnswers()
+        {
+            answerHandler.Add((byte)Answers.AnswerMethods.Login, Answers.Login);
+            answerHandler.Add((byte)Answers.AnswerMethods.Register, Answers.Register);
+            answerHandler.Add((byte)Answers.AnswerMethods.NewCharacter, Answers.NewCharacter);
+        }
+
+    }
+}
