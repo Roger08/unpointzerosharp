b606454d8d6d95bc026b3a5a657d7e7b82c6425f
Roger08 <02dpz2bp@randomail.net>
2014-01-25 21:43:17 +0100

    Second Commit

---
+++ unpointzerosharp/Classes/clsText.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace unpointzerosharp.Classes
+{
+    class clsText
+    {
+        public static void Show(string message)
+        {
+
+        }
+    }
+}
