6cafd76b201626bcf487fb5a27acf740f643ef94
Roger08 <02dpz2bp@randomail.net>
2014-01-26 19:57:39 +0100

    ajout classe player

---
+++ unpointzerosharp/Player.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+
+namespace unpointzerosharp
+{
+    internal class Player
+    {
+        private Socket socket;
+        private Thread thread;
+        string ip;
+        byte status;
+
+        private enum clientStatus
+        { 
+            connected,
+            inGame,
+        }
+
+        internal Player(Socket Sock, Thread Thread)
+        {
+            socket = Sock;
+            thread = Thread;
+
+            status = (byte)clientStatus.connected;
+        }
+    }
+}
